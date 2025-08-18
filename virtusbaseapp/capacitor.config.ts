8a7a02c10f73dc3e43bf679f521b7d28e3f56b73
28jonas <jonasvandecasteele2002@gmail.com>
2025-08-18 18:41:57 +0200

    try to push app

---
+++ virtusbaseapp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'virtusbaseapp',
+  webDir: 'dist'
+};
+
+export default config;
